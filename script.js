3472dc2ccf13df116c7691076fe3c7b764285135
Rahul Kumar Srivastava <124518238+imrahulsrivastava@users.noreply.github.com>
2023-12-22 18:38:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+// Write your script here
+
+document
+  .querySelector("input[type=button]")
+  .addEventListener("click", function () {
+    const colorSelect = document.getElementById("colorSelect");
+    colorSelect.remove(colorSelect.selectedIndex);
+  });
